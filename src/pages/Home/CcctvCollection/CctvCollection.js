458fdf37c72274dd593bcb8ac9633d3bb520f45b
mdshohidulislam1486 <Shohidul Islam>
2021-11-12 11:05:44 +0600

    updated

---
+++ src/pages/Home/CcctvCollection/CctvCollection.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import { Container, TextField } from '@mui/material';
+import Cctv from '../Ccctv/Cctv';
+import { display } from '@mui/system';
+
+
+const CctvCollection = () => {
+
+    const [cctvCol, setCctvCol] = useState([])
+
+    useEffect(()=>{
+    fetch('http://localhost:5000/cctv')
+    .then(res => res.json())
+    .then(data => setCctvCol(data))
+    },[])
+
+    return (
+            <Container sx={{pt:3, display:"flex", flexWrap:'wrap', justifyContent:"center"}} >
+                {
+                    cctvCol.map(mapCCtv =><Cctv key={mapCCtv.id} mapCCtv={mapCCtv}></Cctv>)
+                }
+               
+            </Container>
+    );
+};
+
+export default CctvCollection;
--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../../Shared/Header/Header';
+import CctvCollection from '../CcctvCollection/CctvCollection';
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
+            <CctvCollection></CctvCollection>
         </div>
     );
 };
