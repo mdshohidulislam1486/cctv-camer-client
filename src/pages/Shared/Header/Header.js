7664fd4b7a6b970f391ea51a7e45405e9541439f
mdshohidulislam1486 <Shohidul Islam>
2022-01-10 23:02:00 +0600

    updated

--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -8,40 +8,68 @@ import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import { NavLink } from 'react-router-dom';
 import useAuth from '../../hooks/useAuth';
+import logo from '../../../images/logo.jpg'
+import { makeStyles } from '@mui/styles';
+import { useTheme } from '@mui/material';
 
 
 const Header = () => {
+    const theme = useTheme()
     const {user, logOut} =useAuth()
+    const useStyle = makeStyles({
+        navItem:{
+            [theme.breakpoints.down('sm')]: {
+                display:'none'
+               }
+        },
+    navIcon:{
+        [theme.breakpoints.up('sm')]: {
+           display:'none'
+          }
+    }
+    })
+
+    const {navItem, navIcon} = useStyle()
+
     return (
+    <>
+    
+    
     <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-        <Toolbar>
+      
+        <AppBar position="static" sx={{display:'flex'}}>
+        <MenuIcon
+        sx={{cursor:'pointer'}}
+        className={navIcon}
+        />
+        <Toolbar className={navItem}>
             <IconButton
+            
             size="large"
             edge="start"
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
             >
-            <MenuIcon />
+            
+            <img width='50rem' src={logo} alt="" />
             </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-        
-            </Typography>
-            <NavLink  style={{textDecoration:'none', color:"#fff"}} to='/home'> <Button color="inherit">Home</Button></NavLink>
-            <NavLink style={{textDecoration:'none', color:"#fff"}} to='/ourcollection'> <Button s sx={{textDecoration:"none", borderBottom:'none'}} color="inherit">Our Collection</Button></NavLink>
+            
+           
+            <NavLink className={navItem}  style={{textDecoration:'none', color:"#fff"}} to='/home'> <Button color="inherit">Home</Button></NavLink>
+            <NavLink className={navItem}   style={{textDecoration:'none', color:"#fff"}} to='/ourcollection'> <Button  sx={{textDecoration:"none", borderBottom:'none'}} color="inherit">Our Collection</Button></NavLink>
              {user?.email ?
                 <Box>
-                    
-                    
-                    <NavLink style={{textDecoration:'none', color:'#fff'}} to='/orderManage'><Button color="inherit">Order Manage</Button></NavLink> 
+
+                    <NavLink className={navItem}   style={{textDecoration:'none', color:'#fff'}} to='/orderManage'><Button color="inherit">Order Manage</Button></NavLink> 
                      
                     <Button onClick={logOut} color="inherit">Logout</Button>    
                 </Box>
-               : <NavLink style={{textDecoration:'none', color:'#fff'}} to='/login'><Button color="inherit">Login</Button></NavLink> }
+               : <NavLink className={navItem}   style={{textDecoration:'none', color:'#fff'}} to='/login'><Button color="inherit">Login</Button></NavLink> }
         </Toolbar>
         </AppBar>
     </Box>
+    </>
     )
 };
 
