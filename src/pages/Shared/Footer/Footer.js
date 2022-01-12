89bb488a5974d9db5ea61e1a2537d5db5315e1d4
mdshohidulislam1486 <Shohidul Islam>
2022-01-13 02:05:57 +0600

    updater

--- src/pages/Shared/Footer/Footer.js
+++ src/pages/Shared/Footer/Footer.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import { Box, Container, Typography } from '@mui/material';
 import logo from '../../../images/logo.jpg'
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import ChangeCircleIcon from '@mui/icons-material/ChangeCircle';
+import ViewComfyIcon from '@mui/icons-material/ViewComfy';
+                        
+                        
 
 
 const Footer = () => {
@@ -14,21 +22,62 @@ const Footer = () => {
                 <Typography variant='h4' sx={{fontWeight:'700'}}>More about us</Typography>
             </Box>
             <Box  sx={{ bgcolor:'#263238', pt:2}} >
-            <Container sx={{display:{md:"flex"}, justifyContent:'space-between', flexWrap:'wrap', flexDirection:{md:'row'}}}>
-            <div >
-                <p>Contact</p>
-                <p>PHone: 2332432432</p>
-                <p>Email:cmer@cct.com</p>
-            </div>
-            <div>
-               <h5> Security Solution</h5>
-            </div>
-            <div>
-                <p>Address</p>
-                <p>240/12 East kafrul, Dhaka, 1216</p>
-                <p>Open EveryDay</p>
+            <Container sx={{display:{md:"flex"}, justifyContent:'space-between', flexWrap:'wrap', flexDirection:{md:'row'}, alignItems:'center'}}>
+                <Box >
+                    <Typography sx={{fontFamily:'cursive'}}>
+                        TopRated
+                    </Typography>
+                    <Typography>
+                        Security Solution
+                    </Typography>
+                    <Box sx={{cursor:'pointer'}}>
+                        <TwitterIcon></TwitterIcon>
+                        <FacebookIcon></FacebookIcon>
+                        <LinkedInIcon></LinkedInIcon>
+                        <YouTubeIcon></YouTubeIcon>
+                    </Box>
+                    <Typography>info@ssolution.com</Typography>
+                </Box>
+                <Box>
+                    <Typography>
+                        Our Service Area
+                    </Typography>
+                    <Box sx={{display:'flex',alignItems:'center'}}>
+                        <ChangeCircleIcon></ChangeCircleIcon>
+                        <Typography>Home Solution</Typography>
+                       
+                    </Box>
+                    <Box sx={{display:'flex',alignItems:'center'}}>
+                        <ChangeCircleIcon></ChangeCircleIcon>
+                        <Typography> Public Property management </Typography>
+                    </Box>
+                    <Box sx={{display:'flex',alignItems:'center'}}> 
+                        <ChangeCircleIcon></ChangeCircleIcon>
+                        <Typography> Private Property </Typography>
+                    
+                    </Box>
+                    <Box sx={{display:'flex',alignItems:'center'}}>
+                        <ChangeCircleIcon></ChangeCircleIcon>
+                        <Typography>Corporete 24/7 support</Typography>
+                    </Box>
 
-            </div>
+                </Box>
+                <Box>
+                   <Box sx={{display:'flex', alignItems:'center', maxWidth:'25em'}}>
+                       <ViewComfyIcon sx={{mr:".8rem", color:'#808080'}}></ViewComfyIcon>
+                       <Typography variant='body2' color="#808080">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et, dolorum!</Typography>
+                   </Box>
+                   <Box sx={{display:'flex', alignItems:'center', maxWidth:'25em'}}>
+                       <ViewComfyIcon  sx={{mr:".8rem", color:'#808080'}}></ViewComfyIcon>
+                       <Typography  variant='body2' color="#808080">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et, dolorum!</Typography>
+                   </Box>
+                   <Box sx={{display:'flex', alignItems:'center', maxWidth:'25em'}}>
+                       <ViewComfyIcon sx={{mr:".8rem", color:'#808080'}}></ViewComfyIcon>
+                       <Typography variant='body2' color="#808080">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et, dolorum!</Typography>
+                   </Box>
+                   
+
+                </Box>
             </Container>
           </Box>
         </Box>
